e6d45f6e9c2003c579f44a198f6cfaedd7a200b5
IlhamApriansyah <Ilhamapriansyah471@gmail.com>
2022-02-09 14:21:38 +0700

    commit

--- Random Status.py
+++ Random Status.py
@@ -7,21 +7,18 @@ Job			= ['Sekolah','Guru anak tk','Pengacara','Hikkikomori']
 Hobby		= ['Turu','Main game','Ngoprek']
 Quotes		= ['Jangan semangat!','Teruslah gagal!','Hidupmu gaguna','Asikk']
 
-print('Statusmu hari ini :\n')
 
 def output():
-	print('\n====== ' +	random.choice(Greeting) + ' =======')
+	print('\n====== ' +	random.choice(Greeting) + ' ==============================')
 
-	print('\n====== statusku ' +	random.choice(Status) + ' =======')
+	print('\n====== statusku ' +	random.choice(Status) + ' ====================')
 
-	print('\n====== kelaminku ' +	random.choice(Gender) + ' =======')
+	print('\n====== kelaminku ' +	random.choice(Gender) + ' ====================')
 
-	print('\n====== dan aku punya hobby ' +	random.choice(Hobby) + ' =======')
+	print('\n====== dan aku punya hobby ' +	random.choice(Hobby) + ' =============')
 	
 	print('\n====== untuk saat ini, pekerjaanku ' +	random.choice(Job) + ' =======')
 
-	print('\n Apapun yang sudah kamu lakukan hari ini, ')
-
-	print('\n====== ' + random.choice(Quotes) + ' =======')
-
+	print('\n====== Apapun yang sudah kamu lakukan hari ini, ' + random.choice(Quotes))
+	
 output()
