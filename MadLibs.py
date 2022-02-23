648bb46efc68a43da967f724c11ece8dc7e0bbd5
Ilham Apriansyah <90132863+IlhamApriansyah@users.noreply.github.com>
2022-02-23 15:32:54 +0700

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,25 @@
+
+
+#google = "Anna Belle"
+
+
+#print("search to " + google)
+#print("search to {}".format(google))
+#print(f"search to {google}")
+
+
+so = input("gaming android so : ")
+expression = input("expression : ")
+cherfull = input("cherfull : ")
+famous_game = input("famous Game : ")
+reason = input("reason : ")
+weather = input("weather : ")
+expression2 = input("expression : ")
+google = input("google : ")
+
+madlib = f"Gaming in Android is so {so}! it makes me excited all because \
+i wanna playing with {expression}. stay positive and {cherfull} like you are{famous_game} \
+i just wanna happy because {reason}. well, this is a {weather} and people look like {expression2}\
+why people look {google} soo excited?"
+
+print(madlib)
