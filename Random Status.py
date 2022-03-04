088581ce3487093ac506c6ed6083f84ead0ae6e1
Ilham Apriansyah <ilhamapriansyah471@gmail.com>
2022-03-04 13:39:21 +0700

    penambahan fitur pada Random Status

--- Random Status.py
+++ Random Status.py
@@ -1,24 +1,27 @@
 import random
 
-Status		= ['Psatir','Psarkas','Wibu','Pengoding']
-Greeting	= ['Yooo','hallo','Yahallo','Yahaha']
-Gender		= ['Laki-laki','Perempuan','Ndak punya','LGBTQHD']
-Job			= ['Sekolah','Guru anak tk','Pengacara','Hikkikomori']
+Status		= ['Jomblo','Menikah','Pacaran']
+Greeting	= ['Selamat Pagi','Selamat Siang','Selamat Malam']
+Gender		= ['Laki-laki','Perempuan']
+Job			= ['Sekolah','Guru anak tk','Pengacara','Tidak Ada']
 Hobby		= ['Turu','Main game','Ngoprek']
-Quotes		= ['Jangan semangat!','Teruslah gagal!','Hidupmu gaguna','Asikk']
+Quotes		= ['Jangan semangat!','Teruslah gagal!','Teruslah hidup!','Semangat!']
 
+#input
+Nama =  str(input("Masukan namamu : "))
 
+#output
 def output():
-	print('\n====== ' +	random.choice(Greeting) + ' ==============================')
+	print('\n====== ' +	random.choice(Greeting) + print(Nama) )
 
-	print('\n====== hallo, statusku ' +	random.choice(Status) + ' ====================')
+	print('\n====== hai, statusku saat ini ' +	random.choice(Status) )
 
-	print('\n====== jenis kelaminku adalah ' +	random.choice(Gender) + ' ====================')
+	print('\n====== jenis kelaminku adalah ' +	random.choice(Gender) )
 
-	print('\n====== dan aku punya hobby ' +	random.choice(Hobby) + ' =============')
+	print('\n====== dan aku punya hobby ' +	random.choice(Hobby) )
 	
-	print('\n====== untuk saat ini, pekerjaanku ' +	random.choice(Job) + ' =======')
+	print('\n====== untuk saat ini, pekerjaanku ' +	random.choice(Job) )
 
-	print('\n====== Apapun yang sudah kamu lakukan hari ini, ' + random.choice(Quotes) + ' ==========')
+	print('\n====== Apapun yang sudah kamu lakukan hari ini, ' + random.choice(Quotes) )
 	
 output()
